7a1834b524136b71d6bd9e9ad9c8d018d8857da5
Shivang Kumar <shivangkumar@Shivangs-MacBook-Air.local>
2025-05-27 18:02:59 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+
+class App extends React.Component{
+
+
+  // Handle the rotate event fired from the control component
+
+  // If ok button is clicked,  open the selected component
+
+  // If menu button is clicked, go back to the menu screen
+
+  render(){
+    return (
+      <></>
+    );
+  }
+
+}
+
+export default App;
---
+++ src/components/Screen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './css/screen.css';
+
+
+class Screen extends React.Component{
+
+// Display the sidemenu, coverflow, games,Music etc here
+  render(){
+
+    return (
+      <></>
+    );
+  }
+
+}
+
+export default Screen;
+
